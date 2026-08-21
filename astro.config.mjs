4ee8de93c8859a0a83ca0793fe99487e8acc8394
JordanTate <JordanTateAdds@iCloud.com>
2026-08-21 15:34:19 +0100

    feat: add build configuration for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jordantate.github.io',
+  base: '/dh-driving',
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
